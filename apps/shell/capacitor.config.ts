51b607cae03b224691ccd773cfb1420430df5135
Eric Horodyski <horodyski@ionic.io>
2022-09-21 12:25:51 -0400

    feat: add shell app

---
+++ apps/shell/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'shell',
+  webDir: '../../dist/apps/shell',
+  bundledWebRuntime: false,
+};
+
+export default config;
