01138440df9a94e0b194b29cace8569cce449bd4
Eric Horodyski <horodyski@ionic.io>
2022-09-21 13:49:32 -0400

    feat: add featured-products app

---
+++ apps/featured-products/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'featuredProducts',
+  webDir: '../../dist/apps/featured-products',
+  bundledWebRuntime: false,
+};
+
+export default config;
