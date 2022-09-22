ff597b63ba209252fcd22938181b4ed4b649ac3c
Eric Horodyski <horodyski@ionic.io>
2022-09-22 12:22:45 -0400

    feat(host): add host application

---
+++ apps/host/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'host',
+  webDir: '../../dist/apps/host',
+  bundledWebRuntime: false,
+};
+
+export default config;
---
+++ apps/host/src/app/tab1/tab1.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portals-ecommerce-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+})
+export class Tab1Page {
+  constructor() {}
+}
