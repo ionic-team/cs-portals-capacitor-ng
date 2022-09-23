096b9397a637158da105007c7f0863519b0ba54c
Eric Horodyski <horodyski@ionic.io>
2022-09-23 11:53:00 -0400

    feat(shared): add shared library

--- apps/shell/src/app/shop/shop.page.ts
+++ apps/shell/src/app/shop/shop.page.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { ProductService } from '@portals-ecommerce/shared';
 
 @Component({
   selector: 'portals-ecommerce-shop',
   templateUrl: 'shop.page.html',
   styleUrls: ['shop.page.scss'],
 })
-export class ShopPage {}
+export class ShopPage {
+  constructor(private product: ProductService) {
+    console.log(this.product.user);
+  }
+}
