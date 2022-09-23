75dde84e9061174c228752e27bb1baeef8cf8f62
Eric Horodyski <horodyski@ionic.io>
2022-09-23 09:33:17 -0400

    feat(item-detail): start page

--- apps/shell/src/app/item-detail/item-detail.page.ts
+++ apps/shell/src/app/item-detail/item-detail.page.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'portals-ecommerce-item-detail',
   templateUrl: 'item-detail.page.html',
   styleUrls: ['item-detail.page.scss'],
 })
-export class ItemDetailPage {}
+export class ItemDetailPage implements OnInit {
+  product = {
+    id: 1,
+    title: 'Capacitor Snapback',
+    price: 32,
+    description: 'lorem ipsum',
+  };
+  id = -1;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.id = parseInt(this.route.snapshot.paramMap.get('id') || '0', 10);
+  }
+}
