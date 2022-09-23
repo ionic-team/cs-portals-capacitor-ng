25571546ea4a152d00988c57f13988ebe2bb9166
Eric Horodyski <horodyski@ionic.io>
2022-09-23 09:37:39 -0400

    feat(help): add help MFE

---
+++ apps/help/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'help',
+  webDir: '../../dist/apps/help',
+  bundledWebRuntime: false,
+};
+
+export default config;
